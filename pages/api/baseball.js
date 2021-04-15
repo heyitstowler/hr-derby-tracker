3cbff9fb5edd73f47c7b86f41bd027132945fc9d
Chris Towler <ctowler.518@gmail.com>
2021-04-15 09:35:30 -0400

    Initial commit

---
+++ pages/api/baseball.js
@@ -0,0 +1,39 @@
+import $ from 'cheerio'
+
+import PLAYERS from '../../constants/players'
+
+export async function getHomeRunData() {
+  const url = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=0&type=8&season=2021&month=0&season1=2021&ind=0&team=&rost=&age=&filter=&players=&startdate=&enddate=&page=1_1000'
+  const html = await fetch(url)
+    .then(r => r.text())
+  const rows = Array.from($('.rgMasterTable tbody tr', html)).map(
+    row => row.children.filter(el => el && el.name === 'td')
+  )
+  const playerRows = rows.filter(row => {
+    const name = row[1]?.children?.[0]?.children?.[0]?.data
+    // console.log(name)
+    // const name = row[0]?.[1]?.children?.[0]?.children?.[0]?.data
+    return PLAYERS.includes(name)
+  })
+  const data = playerRows.map(row => {
+    const name = row[1]?.children?.[0]?.children?.[0]?.data
+    const hrs = row[5]?.children?.[0]?.data
+    return { name, hrs }
+  })
+  const map = data.reduce((accum, { name, hrs }) => {
+    accum[name] = hrs
+    return accum
+  }, {})
+  PLAYERS.forEach(p => {
+    if (!map[p]) {
+      map[p] = 0
+    }
+  })
+  console.log(playerRows.length)
+  return map
+}
+
+export default async function handler(req, res) {
+  const data = await getHomeRunData()
+  res.json(data)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,86 @@
 import Head from 'next/head'
+import { useMemo, useState } from 'react';
 import styles from '../styles/Home.module.css'
+import { getHomeRunData } from './api/baseball'
 
-export default function Home() {
+function alphabeticalSort(a, b) {
+  if (a[0] === b[0]) {
+    return 0;
+  }
+  else {
+      return (a[0] < b[0]) ? -1 : 1;
+  }
+}
+
+function byHomeRuns(a, b) {
+  if (a[1] === b[1]) {
+    return 0;
+  }
+  else {
+      return (a[1] > b[1]) ? -1 : 1;
+  }
+}
+
+function toSortedList(data, sortingFn = alphabeticalSort) {
+  const list = Object.entries(data)
+  list.sort(sortingFn)
+  return list
+}
+
+export default function Home({ hrs }) {
+
+  const [sort, setSort] = useState('hrs')
+  const hrList = useMemo(() => {
+    if (sort === 'a-z') {
+      return toSortedList(hrs)
+    } else {
+      return toSortedList(hrs, byHomeRuns)
+    }
+  }, [sort])
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>HR Derby</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <header>
+          <h1>Home Run Derby</h1>
+        </header>
+        <table>
+          <thead>
+            <tr>
+            <td>Name</td>
+            <td>HR</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              hrList.map(([name, hrs]) => (
+                <tr key={name}>
+                  <td>{name}</td>
+                  <td>{hrs}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        
       </footer>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const hrs = await getHomeRunData()
+  return {
+    props: {
+      hrs
+    }
+  }
 }
