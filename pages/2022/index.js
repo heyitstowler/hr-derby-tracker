137443337a9ed6478c1c27ae0c501ae264bba2c1
Christopher Towler <christophertowler@christohersmbp2.myfiosgateway.com>
2022-05-17 13:26:43 -0400

    Fix imports

--- pages/2022/index.js
+++ pages/2022/index.js
@@ -3,6 +3,7 @@ import Layout from '../../components/Layout';
 import Stats from '../../components/Stats';
 import { getHomeRunData } from '../api/baseball'
 import TEAMS from '../../constants/teams-2022';
+import { fetchOptimalTeam } from '../api/knapsack';
 
 export default function Home({ hrs, optimal }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout';
 import Stats from '../components/Stats';
 import TEAMS from '../constants/teams-2022';
 import { getHomeRunData } from './api/baseball'
+import { fetchOptimalTeam } from './api/knapsack';
 
 export default function Home({ hrs, optimal }) {
   return (
